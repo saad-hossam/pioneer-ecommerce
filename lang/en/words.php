7458a31bebeea3b88c89f05c0adf4d07ce542904
Your Name <your.email@example.com>
2026-06-09 08:07:25 +0300

    master project

---
+++ lang/en/words.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'name_en' => 'Name in English',
+    'name_ar' => 'Name in Arabic',
+    'submit' => 'Submit',
+    'name' => 'Name',
+
+
+];
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/********************
+ * Web Routes
+ ********************/
+
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => ['localize', 'localizationRedirect', 'localeSessionRedirect', 'localeViewPath']
+    ],
+    function () {
+        Route::get('/', function () {
+            return view('welcome');
+        });
+
+    }
+);
